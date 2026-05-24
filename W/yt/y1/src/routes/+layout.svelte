1c408d8d71f044a59f871f40a252369ebe48a5c5
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-24 10:29:55 +0000

    ⚠️ w/yt

--- W/yt/y1/src/routes/+layout.svelte
+++ W/yt/y1/src/routes/+layout.svelte
@@ -5,18 +5,19 @@
     "https://res.cloudinary.com/dzma12njh/image/upload/q_auto/f_auto/3.webp"
   const OG_IMAGE_URL =
     "https://res.cloudinary.com/dzma12njh/image/upload/q_auto/f_auto/3.webp"
-  const SITE_URL = "https://mistresscat" // or import from env
-  const SITE_TITLE = "PantySmell"
-  const SITE_DESCRIPTION = "BBW Panty Smelling"
+  const SITE_URL = "https://tinyurl.com/bde2rpr5"
+  const SITE_TITLE = "Panty Smelling"
+  const SITE_DESCRIPTION = "Panty Piss drinking and fart sucking"
 
   let { children } = $props()
 </script>
 
 <svelte:head>
-  <!-- Favicon -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
   <link rel="icon" type="image/webp" href={FAVICON_URL} />
 
-  <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content={SITE_URL} />
   <meta property="og:title" content={SITE_TITLE} />
@@ -24,23 +25,25 @@
   <meta property="og:image" content={OG_IMAGE_URL} />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
-  <meta property="og:image:alt" content="Your site OG image description" />
+  <meta
+    property="og:image:alt"
+    content="Panty Piss drinking and fart sucking"
+  />
   <meta property="og:site_name" content={SITE_TITLE} />
   <meta property="og:locale" content="en_US" />
 
-  <!-- Twitter Card -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:url" content={SITE_URL} />
   <meta name="twitter:title" content={SITE_TITLE} />
   <meta name="twitter:description" content={SITE_DESCRIPTION} />
   <meta name="twitter:image" content={OG_IMAGE_URL} />
-  <meta name="twitter:image:alt" content="SnifingandLicking" />
+  <meta
+    name="twitter:image:alt"
+    content="Panty Piss drinking and fart sucking"
+  />
   <meta name="twitter:site" content="@SmellBoy" />
-  <!-- optional -->
   <meta name="twitter:creator" content="@SmellBoy" />
-  <!-- optional -->
 
-  <!-- Standard SEO (recommended) -->
   <title>{SITE_TITLE}</title>
   <meta name="description" content={SITE_DESCRIPTION} />
   <link rel="canonical" href={SITE_URL} />
