47aa9157f5283cc32f2790763879c6e609dfa456
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-25 07:42:42 +0000

    ⚠️ w/yt

--- W/yt/y1/src/lib/server/db/s2.ts
+++ W/yt/y1/src/lib/server/db/s2.ts
@@ -2,16 +2,18 @@
 // s2.ts - schema is for the tutorial
 // ----------------------------------
 
-import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql, type InferSelectModel } from "drizzle-orm"
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
-export const mistressBook = sqliteTable('mistress_book', {
-    id: integer().primaryKey(),
-    name: text().notNull(),
-    message: text().notNull(),
-    fetish: text().notNull(),
-    country: text(),
-    createdAt: text('created_at', {mode: text})
-    .notNull(),
-    .default(sql'CURRENT_TIMESTAMP')
+export const mistressBook = sqliteTable("mistress_book", {
+  id: integer().primaryKey(),
+  name: text().notNull(),
+  message: text().notNull(),
+  fetish: text().notNull(),
+  country: text(),
+  createdAt: text("created_at", { mode: "text" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
 })
+
+export type MistressBook = InferSelectModel<typeof mistressBook>
