68ed36eab44c07fc7395a67de35a6be2bcd81186
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-25 08:13:11 +0000

    ⚠️ w/yt

--- W/yt/y1/drizzle.config.ts
+++ W/yt/y1/drizzle.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "drizzle-kit"
 
 if (!process.env.DATABASE_URL)
-  throw new Error("BASTARD ! Local DATABASE_URL not set")
+  throw new Error("😡 BASTARD ! Local DATABASE_URL not set")
 
 export default defineConfig({
-  schema: "./src/lib/server/db/schema/**/*.ts",
+  schema: "./src/lib/server/db/**/*.ts",
   dialect: "sqlite",
+  out: "./src/lib/server/db/migrations",
   dbCredentials: { url: process.env.DATABASE_URL },
   verbose: true,
   strict: true,
