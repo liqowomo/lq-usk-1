3d71da1226e4ac6239504f68fb48059c2863bbd5
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-26 17:14:08 +0000

    🍑 MS5

--- W/MS5/m2/src/routes/+page.svelte
+++ W/MS5/m2/src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script>
+  function onclick() {
+    console.log("Booty Smell")
+  }
+
+  function onclick2() {
+    number++
+  }
+
   let numberOne = 69
+  let number = $state(0)
 </script>
 
 <!-- All Components in here -->
 <div class="page-wrapper">
   <h1>Welcome to SvelteKit</h1>
-  <p>This page has its own dark background, other pages won't</p>
+  <p>Testing number variable : {numberOne}</p>
+  <p>Testing number variable : {number}</p>
+
+  <button {onclick}> Lickme </button>
+  
+  <button {onclick2}> Lickme </button>
 </div>
 
 <style>
@@ -14,15 +28,16 @@
     min-height: 100vh;
     /* display: flex; */
     align-items: center;
-    justify-content: center;
+    /* justify-content: space-between; */
     flex-direction: column;
     color: #6aece1;
+    padding: 5rem;
   }
 
   /* This CSS only applies to THIS page */
   h1 {
     color: white;
-    font-size: 5rem;
+    font-size: 2rem;
     font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   }
 </style>
