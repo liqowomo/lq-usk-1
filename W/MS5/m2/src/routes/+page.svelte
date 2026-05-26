e4a0eea78f81edc069113e5ffbbbf71fccecc549
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-26 17:34:49 +0000

    🍑 MS5

--- W/MS5/m2/src/routes/+page.svelte
+++ W/MS5/m2/src/routes/+page.svelte
@@ -1,40 +1,52 @@
 <script>
-  function onclick() {
+  let numberOne = 0
+  let number = $state(0)
+
+  //   Drived syntax - Pased on existing state
+  let userInformation = $derived.by(() => calculateUserInformation())
+
+  function onclick2() {
     console.log("Booty Smell")
   }
 
-  function onclick2() {
+  function onclick() {
     number++
   }
 
-  let numberOne = 69
-  let number = $state(0)
+  //   Note this function is going into userInformation which has derived.by() function
+  function calculateUserInformation() {
+    if (number === 0) {
+      return "Click button Pleae"
+    }
+    if (number === 1) {
+      return `You clicked exactly one time`
+    }
+    return `You clicked ${number} times`
+  }
 </script>
 
-<!-- All Components in here -->
 <div class="page-wrapper">
   <h1>Welcome to SvelteKit</h1>
   <p>Testing number variable : {numberOne}</p>
+
   <p>Testing number variable : {number}</p>
 
-  <button {onclick}> Lickme </button>
-  
-  <button {onclick2}> Lickme </button>
+  <button {onclick}>Lickme</button>
+  <!-- <button {onclick2}>Lickme</button> -->
+
+  <p>{userInformation}</p>
 </div>
 
 <style>
   .page-wrapper {
     background: #091413;
     min-height: 100vh;
-    /* display: flex; */
     align-items: center;
-    /* justify-content: space-between; */
     flex-direction: column;
     color: #6aece1;
     padding: 5rem;
   }
 
-  /* This CSS only applies to THIS page */
   h1 {
     color: white;
     font-size: 2rem;
