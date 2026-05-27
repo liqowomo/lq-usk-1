a2f0cc62efdb091e04fd3cb7b3d10fe96fc93cbe
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-27 16:33:26 +0000

    🍑 MS5

--- W/MS5/m2/src/routes/p4/+page.svelte
+++ W/MS5/m2/src/routes/p4/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   import UserInput from "$lib/co/UserInput.svelte"
+
+  let data = $props()
+  $inspect(data)
 </script>
 
 <UserInput userName="Fat Aunti"><h1>Put In her Azz</h1></UserInput>
