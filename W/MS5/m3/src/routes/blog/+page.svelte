8303109f383fa5ca34590bd7578124bfe5a7857b
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-28 17:08:50 +0000

    🍑 MS5

--- W/MS5/m3/src/routes/blog/+page.svelte
+++ W/MS5/m3/src/routes/blog/+page.svelte
@@ -5,6 +5,12 @@
   </nav>
 
   <h1 class="h1">Booty Dance</h1>
+  <p>
+    <i>
+      Note you are not using BaseMeta.svelte to change the title, something to
+      look at later</i
+    >
+  </p>
 </div>
 
 <style>
