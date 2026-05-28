a93ca9276871e41dd9580529ee96a5de838cc2aa
liqowomo <123171512+liqowomo@users.noreply.github.com>
2026-05-28 17:00:33 +0000

    🫦

--- W/MS5/m3/src/routes/+page.svelte
+++ W/MS5/m3/src/routes/+page.svelte
@@ -3,6 +3,12 @@
 </script>
 
 <div class="flex">
+  <!-- Route Links -->
+  <nav>
+    <a href="/blog"> ✒️Blog </a>
+  </nav>
+
+  <!-- Content -->
   <h1>Welcome to SvelteKit</h1>
   <p>
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
---
+++ W/MS5/m3/src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<div class="flex">
+  <!-- Route Links -->
+  <nav>
+    <a href="/"> 👙 Home </a>
+  </nav>
+
+  <h1 class="h1">Booty Dance</h1>
+</div>
+
+<style>
+  .h1 {
+    color: #bef264;
+  }
+</style>
